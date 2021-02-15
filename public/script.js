356cd6e68b42e25836ddd6cabb704ecbf5180d69
Sagiv Mishaan <sagivmishaan@gmail.com>
2021-02-15 11:49:39 +0200

    update .js path

--- public/script.js
+++ public/script.js
@@ -1,26 +1,26 @@
 "use strict";
 
-const main = document.querySelector("main");
-const overlay = document.querySelector(".overlay");
-const modal = document.querySelector(".modal");
-const button_enter = document.getElementById("button--0");
-const body = document.querySelector("body");
-const btnCloseCV = document.getElementById("button--x");
-const btnCloseSkills = document.getElementById("button--xs");
-const btnCloseObj = document.getElementById("button--xo");
-const button_git = document.getElementById("button--2");
-const button_linked = document.getElementById("button--1");
-const button_gmail = document.getElementById("img--gmail");
-const skills_modal = document.getElementById("skills--modal");
-const button_skills = document.getElementById("button--3");
-const button_obj = document.getElementById("button--4");
-const cv_modal = document.getElementById("cv--modal");
-const objective_modal = document.getElementById("objective--modal");
+let main_el = document.querySelector("main");
+let overlay_el = document.querySelector(".overlay");
+let modal_el = document.querySelector(".modal");
+let button_enter = document.getElementById("button--0");
+let body = document.querySelector("body");
+let btnCloseCV = document.getElementById("button--x");
+let btnCloseSkills = document.getElementById("button--xs");
+let btnCloseObj = document.getElementById("button--xo");
+let button_git = document.getElementById("button--2");
+let button_linked = document.getElementById("button--1");
+let button_gmail = document.getElementById("img--gmail");
+let skills_modal = document.getElementById("skills--modal");
+let button_skills = document.getElementById("button--3");
+let button_obj = document.getElementById("button--4");
+let cv_modal = document.getElementById("cv--modal");
+let objective_modal = document.getElementById("objective--modal");
 
 let openObjectModal = function () {
   objective_modal.classList.remove("hidden");
   button_skills.classList.add("hidden");
-  overlay.classList.remove("hidden");
+  overlay_el.classList.remove("hidden");
   button_enter.classList.add("hidden");
   button_git.classList.add("hidden");
   body.classList.add("overlay");
@@ -30,7 +30,7 @@ let openObjectModal = function () {
 let popUpCV = function () {
   cv_modal.classList.remove("hidden");
   button_skills.classList.add("hidden");
-  overlay.classList.remove("hidden");
+  overlay_el.classList.remove("hidden");
   button_enter.classList.add("hidden");
   button_obj.classList.add("hidden");
   button_git.classList.add("hidden");
@@ -39,11 +39,11 @@ let popUpCV = function () {
   button_gmail.classList.add("hidden");
 };
 
-const closeModal = function () {
+let closeModal = function () {
   objective_modal.classList.add("hidden");
   skills_modal.classList.add("hidden");
   cv_modal.classList.add("hidden");
-  overlay.classList.add("hidden");
+  overlay_el.classList.add("hidden");
   button_skills.classList.remove("hidden");
   button_obj.classList.remove("hidden");
   body.classList.remove("overlay");
@@ -52,7 +52,7 @@ const closeModal = function () {
   button_git.classList.remove("hidden");
   button_linked.classList.remove("hidden");
 };
-const escBtn = function (event) {
+let escBtn = function (event) {
   if (event.key === "Escape") {
     if (!cv_modal.classList.contains("hidden")) closeModal();
     if (!skills_modal.classList.contains("hidden")) closeModal();
@@ -60,7 +60,7 @@ const escBtn = function (event) {
   }
 };
 
-const openSkillsModal = function () {
+let openSkillsModal = function () {
   skills_modal.classList.remove("hidden");
   button_enter.classList.add("hidden");
   button_git.classList.add("hidden");
@@ -70,8 +70,8 @@ const openSkillsModal = function () {
   button_gmail.classList.add("hidden");
   // button_skills.classList.add('hidden');
 };
-const openCloseModalSkill = function (e) {
-  console.log(e.target.innerHTML == "skills");
+let openCloseModalSkill = function (e) {
+  console.log(e.target.innerHTML === "skills");
   if (!skills_modal.classList.contains("hidden")) {
     skills_modal.classList.toggle("hidden");
     button_skills.classList.toggle("hidden");
@@ -82,7 +82,7 @@ const openCloseModalSkill = function (e) {
     button_enter.classList.toggle("hidden");
     body.classList.toggle("overlay");
   }
-  if (e.target.innerHTML == "skills") {
+  if (e.target.innerHTML === "skills") {
     button_obj.classList.toggle("hidden");
     skills_modal.classList.toggle("hidden");
     button_skills.classList.toggle("hidden");
@@ -100,5 +100,5 @@ if (document) document.addEventListener("keydown", escBtn);
 if (btnCloseCV) btnCloseCV.addEventListener("click", closeModal);
 if (btnCloseSkills) btnCloseSkills.addEventListener("click", closeModal);
 if (btnCloseObj) btnCloseObj.addEventListener("click", closeModal);
-if (overlay) overlay.addEventListener("click", closeModal);
-if (main) main.addEventListener("click", openCloseModalSkill);
+if (overlay_el) overlay_el.addEventListener("click", closeModal);
+if (main_el) main_el.addEventListener("click", openCloseModalSkill);
